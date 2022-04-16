3da60b0e52fd6d6f058cb4073b0cab763d773a3d
Nader Elhaj <naderelhaj3@gmail.com>
2022-04-16 19:46:13 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      {/* Left */}
+      <div>
+        <Image
+          src="/images/instagram.png"
+          layout='fill'
+        />
+      </div>
+      {/* Middle */}
+
+      {/* Right */}
+    </div>
+  )
+}
+
+export default Header
