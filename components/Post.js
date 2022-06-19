72b1bb31f13f87bb8fafc9365f77e00a05f76c2a
Nader Elhaj <naderelhaj3@gmail.com>
2022-06-19 22:55:13 +0100

    Implementing Like Functionality

--- components/Post.js
+++ components/Post.js
@@ -12,10 +12,13 @@ import { useSession } from 'next-auth/react'
 import {
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   onSnapshot,
   orderBy,
   query,
   serverTimestamp,
+  setDoc,
 } from 'firebase/firestore'
 import { db } from '../firebase'
 import Moment from 'react-moment'
@@ -23,17 +26,45 @@ function Post({ id, username, userImage, img, caption }) {
   const { data: session } = useSession()
   const [comment, setComment] = useState('')
   const [comments, setComments] = useState([])
+  const [likes, setLikes] = useState([])
+  const [hasLiked, setHasLiked] = useState(false)
 
   useEffect(
     () =>
       onSnapshot(
         query(
-          collection(db, "posts", id, "comments"), orderBy("timestamp", "desc")),
-          (snapshot) => setComments(snapshot.docs)
-        
+          collection(db, 'posts', id, 'comments'),
+          orderBy('timestamp', 'desc')
+        ),
+        (snapshot) => setComments(snapshot.docs)
       ),
-    []
+    [db, id]
   )
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  )
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.id))
+    } else {
+      if(doc && db){
+        await setDoc(doc(db, 'posts', id, 'likes', session?.user?.id), {
+          username: session.user.username,
+        })
+      }
+    }
+  }
+  useEffect
+  (() => 
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user?.id) !== -1
+    )
+  , [likes])
 
   const sendComment = async (e) => {
     e.preventDefault()
@@ -62,7 +93,14 @@ function Post({ id, username, userImage, img, caption }) {
       {session && (
         <div className="flex justify-between p-4 px-4">
           <div className="flex space-x-4  ">
-            <HeartIcon className="btn" />
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="btn text-red-500"
+              />
+            ) : (
+              <HeartIcon className="btn" onClick={ likePost} />
+            )}
             <ChatIcon className="btn" />
             <PaperAirplaneIcon className="btn" />
           </div>
@@ -71,24 +109,33 @@ function Post({ id, username, userImage, img, caption }) {
       )}
 
       <p className="truncate p-5">
+        {likes.length >0 && (
+          <span className='font-bold mb-1 '>{likes.length} likes <br /></span>
+        )}
         <span className="mr-1 font-bold">{username} </span>
         {caption}
       </p>
 
-        {comments.length>0 &&(
-          <div className='ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin'>
-            {comments.map((comment)=>(
-              <div key={comment.id} className="flex items-center space-x-2 mb-3">
-                <img src={comment.data().profileImg} alt="" className='h-7 rounded-full object-cover'/>
-                <p className='text-sm flex-1'><span className='font-bold'>{comment.data().username} </span>{comment.data().comment}</p>
-                <Moment  fromNow className="py-5 text-cs" >
-                  {comment.data().timestamp?.toDate()}
-                </Moment>
-              </div>
-            ))}
-          </div>
-        )}
-
+      {comments.length > 0 && (
+        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thin scrollbar-thumb-black">
+          {comments.map((comment) => (
+            <div key={comment.id} className="mb-3 flex items-center space-x-2">
+              <img
+                src={comment.data().profileImg}
+                alt=""
+                className="h-7 rounded-full object-cover"
+              />
+              <p className="flex-1 text-sm">
+                <span className="font-bold">{comment.data().username} </span>
+                {comment.data().comment}
+              </p>
+              <Moment fromNow className="text-cs py-5">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
       {session && (
         <form className="flex items-center p-4">
